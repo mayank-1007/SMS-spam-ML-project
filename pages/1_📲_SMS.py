19cadee020d9ab8a0c5c793b39e78ed68afc5987
mayank-1007 <mayankmanchanda2005@gmail.com>
2024-07-17 10:54:20 +0530

    -

--- pages/1_📲_SMS.py
+++ pages/1_📲_SMS.py
@@ -24,22 +24,22 @@ def transform_text(text):
     for i in text:
         y.append(ps.stem(i)) # to stem down loving to love and remove e and ings etc.
     return " ".join(y)
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+# tfidf = pickle.load(open('vectorizer.pkl','rb'))
+# model = pickle.load(open('model.pkl','rb'))
 
-st.title("SMS Spam Dection")
+# st.title("SMS Spam Dection")
 
-input_sms = st.text_area("Enter the message")
+# input_sms = st.text_area("Enter the message")
 
-if st.button('Predict'):
-    # preprocess
-    transformed_sms = transform_text(input_sms)
-    #vectorize
-    vector_input = tfidf.transform([transformed_sms])
-    # predict
-    result = model.predict(vector_input)[0]
-    #display result
-    if result ==1:
-        st.header("SPAM!")
-    else:
-        st.header("NOT SPAM!")
+# if st.button('Predict'):
+#     # preprocess
+#     transformed_sms = transform_text(input_sms)
+#     #vectorize
+#     vector_input = tfidf.transform([transformed_sms])
+#     # predict
+#     result = model.predict(vector_input)[0]
+#     #display result
+#     if result ==1:
+#         st.header("SPAM!")
+#     else:
+#         st.header("NOT SPAM!")
