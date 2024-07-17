64ea317632c4cbda2fb531b5c0efa9710afa9b8a
mayank-1007 <mayankmanchanda2005@gmail.com>
2024-07-17 10:59:14 +0530

    -

--- pages/1_📲_SMS.py
+++ pages/1_📲_SMS.py
@@ -25,15 +25,15 @@ def transform_text(text):
         y.append(ps.stem(i)) # to stem down loving to love and remove e and ings etc.
     return " ".join(y)
 # tfidf = pickle.load(open('vectorizer.pkl','rb'))
-# model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
-# st.title("SMS Spam Dection")
+st.title("SMS Spam Dection")
 
-# input_sms = st.text_area("Enter the message")
+input_sms = st.text_area("Enter the message")
 
-# if st.button('Predict'):
-#     # preprocess
-#     transformed_sms = transform_text(input_sms)
+if st.button('Predict'):
+    # preprocess
+    transformed_sms = transform_text(input_sms)
 #     #vectorize
 #     vector_input = tfidf.transform([transformed_sms])
 #     # predict
@@ -43,3 +43,4 @@ def transform_text(text):
 #         st.header("SPAM!")
 #     else:
 #         st.header("NOT SPAM!")
+
